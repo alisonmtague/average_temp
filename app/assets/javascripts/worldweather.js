9279edd6e5972ece677cf33236e2bdb04cf602ff
Alison Tague <alisonmtague@gmail.com>
2015-05-12 14:20:55 -0400

    fixing parsing function

--- app/assets/javascripts/worldweather.js
+++ app/assets/javascripts/worldweather.js
@@ -9,8 +9,22 @@ $(document).ready(function(){
 			dataType: "jsonp",
 			method: "GET",
 			success: function(response){
-				console.log(response);
+				// console.log(response);
+				displayResults(response, input);
 			}
 		})
+		this.reset();
 	})
-})
+});
+
+function displayResults(response, input){
+	var apikey = "a531df13fffd41b9bd2039255bb77";
+	$.getJSON("http://api.worldweatheronline.com/free/v2/weather.ashx" + "?key=" + apikey + "&q=" + input + "&format=json" + "&num_of_results=1", function(data){
+		var current_condition = data['data']['current_condition'];
+		for (var i=0; i < current_condition.length; i++){
+			var temp_f = current_condition[i][0]['temp_F'];
+			var temp_c = current_condition[i][0]['temp_C'];
+		}
+	})
+		console.log(temp_f);
+}
