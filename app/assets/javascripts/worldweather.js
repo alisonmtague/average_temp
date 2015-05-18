95b58916dda7f4b9c65d24c741f2fa77f62b2967
Alison Tague <alisonmtague@gmail.com>
2015-05-18 12:17:04 -0400

    Styled

--- app/assets/javascripts/worldweather.js
+++ app/assets/javascripts/worldweather.js
@@ -9,33 +9,37 @@ $(document).ready(function(){
 			dataType: "jsonp",
 			method: "GET",
 			success: function(parsed_json){
-				temp_f = parsed_json['data']['current_condition'][0]['temp_F'];
-				temp_c = parsed_json['data']['current_condition'][0]['temp_C'];
+				var temp_f = parsed_json['data']['current_condition'][0]['temp_F'];
+				var temp_c = parsed_json['data']['current_condition'][0]['temp_C'];
 				console.log(temp_f);
 				console.log(temp_c);
-				$("#temp-f h3").text(temp_f + "°F")
-				$("#temp-c h3").text(temp_c + "°C")
+				$("#temp-f h3").text(temp_f + "°F").css('padding', '60px')
+				$("#temp-c h3").text(temp_c + "°C").css('padding', '40px')
 			}
 		})
 		var apikey_openweather = "534f609c3e5bb2f40d98247a489b29a1";
 		var url_openweather = "http://api.openweathermap.org/data/2.5/weather?zip=" + input + "&APPID=" + apikey_openweather ;
+		
 		$.ajax({
 			url: url_openweather,
 			dataType: "jsonp",
 			method: "GET",
 			success: function(parsed_json){
+				var name = parsed_json['name'];
 				var temp_kelvin = parsed_json['main']['temp'];
-				temp_f2 = ((temp_kelvin - 273.15)*1.8000 + 32.00);
-				temp_c2 = (temp_kelvin - 273.15 + " celsius");
-				console.log((temp_kelvin - 273.15)*1.8000 + 32.00);
-				console.log(temp_kelvin - 273.15 + " celsius");
+				var temp_f2 = ((temp_kelvin - 273.15)*1.8000 + 32.00);
+				var temp_c2 = (temp_kelvin - 273.15);
+				console.log(temp_f2);
+				console.log(temp_c2);
+				$("#name h2").text(name);
 			}
 		})
-	var average = function(x, y){
-		return (x + y)/2;
-	}
-	console.log(average(temp_f, temp_f2));
-	console.log(average(temp_c, temp_c2));
+	// var average = function(x, y){
+	// 	return (x + y)/2;
+	// }
+	// console.log(average(temp_f, temp_f2));
+	// console.log(average(temp_c, temp_c2));
+	// console.log(temp_f2);
 		this.reset();
 	})
 });
